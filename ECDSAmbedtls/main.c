52119547632238af127a45005c2c6e2a7254afcf
Varshith Pawar H R <varshithpawarhr@gmail.com>
2025-04-06 22:10:25 +0530

    ecdsa successfull implementation

--- ECDSAmbedtls/main.c
+++ ECDSAmbedtls/main.c
@@ -1,22 +1,43 @@
-
-//ADD THE DAMN IMPORT STATEMENTS THAT YOU NEED
-
-#if defined (CY_USING_HAL)
 #include "cyhal.h"
-#endif
 #include "cybsp.h"
 #include "cy_retarget_io.h"
-#include "ecp.h"
-#include "mbedtls/entropy.h"
-#include "ctr_drbg.h"
 #include "mbedtls/ecdsa.h"
-#include "mbedtls/ecp.h"
-#include "mbedtls/entropy.h"
 #include "mbedtls/ctr_drbg.h"
+#include "mbedtls/entropy.h"
 #include "mbedtls/sha256.h"
 
+/**************************
+* Macros
+***************************/
 
 
+/***************************
+* Global Variables
+***************************/
+
+
+/***************************
+* Function Prototypes
+***************************/
+
+
+/***************************
+* Function Definitions
+***************************/
+/***************************
+* Function Name: print_uint8_data()
+****************************
+* Summary:
+*   Function used to display the data in hexadecimal format
+*
+* Parameters:
+*  uint8_t* data - Pointer to location of data to be printed
+*  size_t  len  - length of data to be printed
+*
+* Return:
+*  void
+*
+***************************/
 void print_uint8_data(uint8_t* data, size_t len)
 {
     char print[10];
@@ -32,9 +53,9 @@ void print_uint8_data(uint8_t* data, size_t len)
     printf("\r\n");
 }
 
-/*******************************************************************************
+/***************************
  * Function Name: print_mpi_data()
- ********************************************************************************
+ ****************************
  * Summary:
  *  Prints the contents of an mbedtls_mpi structure in hexadecimal format.
  *
@@ -45,18 +66,18 @@ void print_uint8_data(uint8_t* data, size_t len)
  * Return:
  *  void
  *
- *******************************************************************************/
+ ***************************/
 void print_mpi_data(mbedtls_mpi* data)
 {
-	size_t len = mbedtls_mpi_size(data);
-	unsigned char buffer[100] = {0};
+    size_t len = mbedtls_mpi_size(data);
+    unsigned char buffer[100] = {0};
     mbedtls_mpi_write_binary(data, buffer, len);
     print_uint8_data(buffer, len);
 }
 
-/*******************************************************************************
+/***************************
  * Function Name: print_ecp_point_data()
- ********************************************************************************
+ ****************************
  * Summary:
  *  Prints the contents of an mbedtls_ecp_point structure in
  *  uncompressed binary format.
@@ -69,24 +90,23 @@ void print_mpi_data(mbedtls_mpi* data)
  * Return:
  *  void
  *
- ******************************************************************************/
+ **************************/
 void print_ecp_point_data(mbedtls_ecp_point* data, mbedtls_ecp_group *grp)
 {
-	unsigned char buffer[100] = {0};
-	size_t buflen = 0; //ECP_KEY_LENGTH
+    unsigned char buffer[100] = {0};
+    size_t buflen = 0; //ECP_KEY_LENGTH
 
     mbedtls_ecp_point_write_binary(grp, data, MBEDTLS_ECP_PF_UNCOMPRESSED,
-    		&buflen, buffer, sizeof(buffer));
+            &buflen, buffer, sizeof(buffer));
     print_uint8_data(buffer, buflen);
 }
 
-/*******************************************************************************
+/***************************
 * Function Name: main
-*********************************************************************************
+***************************
 * Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
+* This is the main function for CPU. It generates an ECDSA keypair,
+* signs a message, and verifies the signature.
 *
 * Parameters:
 *  void
@@ -94,11 +114,10 @@ void print_ecp_point_data(mbedtls_ecp_point* data, mbedtls_ecp_group *grp)
 * Return:
 *  int
 *
-*******************************************************************************/
+***************************/
 int main(void)
 {
     cy_rslt_t result;
-
 
     /* Initialize the device and board peripherals */
     result = cybsp_init();
@@ -113,93 +132,80 @@ int main(void)
     __enable_irq();
 
     /* Initialize retarget-io to use the debug UART port */
-    result = cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
-    		CY_RETARGET_IO_BAUDRATE);
+    result = cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX, CY_RETARGET_IO_BAUDRATE);
 
     /* UART port init failed. Stop program execution */
-	if (result != CY_RSLT_SUCCESS)
-	{
-	   CY_ASSERT(0);
-	}
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
 
-	printf("\x1b[2J\x1b[;H");
-
-
-	//CORE WORKING OF MY PROGRAM
-
-	printf("\x1b[2J\x1b[;H");
-	    int ret;
-	    const char *message = "Hello, ECDSA!";
-	    unsigned char hash[32]; // SHA-256 hash
-
-	    mbedtls_ecp_group grp;
-	    mbedtls_mpi d, r, s;
-	    mbedtls_ecp_point Q;
-	    mbedtls_entropy_context entropy;
-	    mbedtls_ctr_drbg_context ctr_drbg;
-	    const char *pers = "ecdsa_demo";
-
-	    // Init
-	    mbedtls_ecp_group_init(&grp);
-	    mbedtls_ecp_point_init(&Q);
-	    mbedtls_mpi_init(&d);
-	    mbedtls_mpi_init(&r);
-	    mbedtls_mpi_init(&s);
-	    mbedtls_entropy_init(&entropy);
-	    mbedtls_ctr_drbg_init(&ctr_drbg);
-
-	    // Seed RNG
-	    mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
-	                                 (const unsigned char *) pers, strlen(pers));
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
 
 
-	    // Load elliptic curve
-	    mbedtls_ecp_group_load(&grp, MBEDTLS_ECP_DP_SECP256R1);
 
-	    // Generate keypair
-	    mbedtls_ecp_gen_keypair(&grp, &d, &Q, mbedtls_ctr_drbg_random, &ctr_drbg);
+    mbedtls_ctr_drbg_context random_context;
+    mbedtls_entropy_context entropy_context;
 
-	    // Hash message
-	    mbedtls_sha256((const unsigned char *)message, strlen(message), hash, 0);
+    /* Initialize the random number generator */
+    mbedtls_ctr_drbg_init(&random_context);
+    mbedtls_entropy_init(&entropy_context);
 
-	    // Sign (deterministic)
-	    ret = mbedtls_ecdsa_sign_det_ext(
-	        &grp, &r, &s, &d, hash, sizeof(hash),
-	        MBEDTLS_MD_SHA256,
-	        mbedtls_ctr_drbg_random, &ctr_drbg
-	    );
+    /* Seed the random number generator */
+    mbedtls_ctr_drbg_seed(&random_context, mbedtls_entropy_func, &entropy_context, (const unsigned char *)"hello", 5);
 
-	    if (ret != 0) {
-	        printf("Signing failed");
-	        return ret;
-	    }
+    /* Initialize ECDSA context */
+    mbedtls_ecp_keypair keypair;
+    mbedtls_mpi r, s;
+    mbedtls_ecp_keypair_init(&keypair);
+    mbedtls_mpi_init(&r);
+    mbedtls_mpi_init(&s);
 
-	    printf("Signature generated!\n");
 
-	    // Verify
-	    ret = mbedtls_ecdsa_verify(&grp, hash, sizeof(hash), &Q, &r, &s);
 
-	    if (ret == 0) {
-	        printf("Signature verified successfully.\n");
-	    } else {
-	        printf("Signature verification failed");
-	    }
-
-	    // Cleanup
-	    mbedtls_ecp_group_free(&grp);
-	    mbedtls_ecp_point_free(&Q);
-	    mbedtls_mpi_free(&d);
-	    mbedtls_mpi_free(&r);
-	    mbedtls_mpi_free(&s);
-	    mbedtls_ctr_drbg_free(&ctr_drbg);
-	    mbedtls_entropy_free(&entropy);
+    /* Generate ECDSA keypair */
+ mbedtls_ecp_group_load(&keypair.private_grp, MBEDTLS_ECP_DP_SECP256R1); // not needed (required if the things are on different interface)
+ mbedtls_ecp_gen_keypair(&keypair.private_grp, &keypair.private_d, &keypair.private_Q, mbedtls_ctr_drbg_random, &random_context);
 
 
 
 
 
+    /* Hash the message */
+    const char *message = "Yokoso Watashi no soul society Ye!";
+    uint8_t hash[32];
+    mbedtls_sha256((const unsigned char *)message, strlen(message), hash, 0);
+
+
+
+    /* Sign the hash */
+    result = mbedtls_ecdsa_sign(&keypair.private_grp, &r, &s, &keypair.private_d, hash, sizeof(hash), mbedtls_ctr_drbg_random, &random_context);
+
+
+    /* Verify the signature */
+    int ret = mbedtls_ecdsa_verify(&keypair.private_grp, hash, sizeof(hash), &keypair.private_Q, &r, &s);
+    printf("Signature verification result: %d\r\n", ret);
+
+    if (ret == 0)
+    {
+        printf("Signature verified successfully.\r\n");
+    }
+    else
+    {
+        printf("Signature verification failed.\r\n");
+    }
+
+    /* Free resources */
+    mbedtls_ctr_drbg_free(&random_context);
+    mbedtls_entropy_free(&entropy_context);
+    mbedtls_ecp_keypair_free(&keypair);
+    mbedtls_mpi_free(&r);
+    mbedtls_mpi_free(&s);
 
     for (;;)
     {
     }
 }
+
+
